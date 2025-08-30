a5354655f7501927199d602a272a16f9e30f3fc3
diya-2104 <ranadiya2104@gmail.com>
2025-08-30 09:05:31 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Assignment02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment02.Controllers
+{
+
+    public class UserController : Controller
+    {
+        private static List<User> users = new List<User>();
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            user.Id = users.Count + 1;
+            users.Add(user);
+            return PartialView("_UserList", users);
+        }
+    }
+}
